f8fa127ef7244f271f9fa4c195ddffc3b53e877e
escobar2018 <antoine.escobar@ensiie.fr>
2019-05-03 15:40:03 +0200

    fonction ville la plus proche ok

--- projet_av.py
+++ projet_av.py
@@ -1,7 +1,8 @@
+
 from mpmath import *
 import csv
 import scipy
-import numpy
+import numpy as np
 
 
 # Importe le fichier.csv et le stock dans un tableau ville
@@ -25,15 +26,40 @@ del ville[0]
 # FONCTION : 
 # ensures : Calcul la distance euclidienne entre deux villes
 # input   : ville de depart et ville d'arrivée, en tuple(x,y)
-# return  : distance en float
+# return  : float | distance
 def distance(depart, arrivee):
     dist = (float(depart[0])-float(arrivee[0]))**2 + (float(depart[1])-float(arrivee[1]))**2
     dist = sqrt(dist)
     return dist
 
-parcours = []
-for vertice in ville:
-    teste = distance(ville[0],vertice)
-    parcours.append(teste)
 
-print(parcours)
+# FONCTION
+# ensures : Calcule la distance de chaque chemin possible depuis la 1ere ville
+# input   : liste des villes
+# return  : float | liste de distances
+def listeDistance(ville):
+    parcours = []
+    for vertice in ville:
+        teste = distance(ville[0],vertice)
+        if(teste != 0.0):
+            parcours.append(teste)
+    return parcours
+
+# FONCTION  
+# ensures ; Trouve la ville la plus proches de la 1ere ville
+# input   : liste des villes
+# return  : integer | indice de la ville la plus proche dans la liste "ville"
+def numVillePlusProche(ville):
+    dist = []
+    ville = np.array(ville)
+    for (indice, sommet) in enumerate(ville):
+        toto = distance(ville[0],sommet)
+        if(toto != 0.0):
+            dist.append(toto)
+    solu = dist.index(min(dist)) + 1
+    return solu
+
+print(ville)
+print(listeDistance(ville))
+print(numVillePlusProche(ville))
+
