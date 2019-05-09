4a51056a7a2088190ca8b09eba52268441aeede3
a2lino <araujo_adelino@orange.fr>
2019-05-09 23:10:27 +0200

    - actualisation du main - rangement listes tests bon fonctionnement

--- mainSolutionOptimale.py
+++ mainSolutionOptimale.py
@@ -13,40 +13,57 @@ from fonctionsSolutionOptimale import *
 #################################################################
 
 
-############## EXPLOITATION DU FICHIER CSV ##########################
+####### EXPLOITATION DU FICHIER CSV ###########
 
 #Ouvre le fichier CSV et attribue ses 2 RETURNS aux variables "ville" et "nbVille"
 ville, nbVille = exploitationFichierCSV('test10.csv')
 
 
 
-parcours = []
-parcours.append(0)
+####### CREATION DE LA LISTE DE TOUS LES CHEMINS POSSIBLES et de leur nombre ########
+cheminsPossibles, nombreCheminPossibles = listeCheminsPossibles(ville)
 
-sauve = ville
+
+
+############### TESTS DE FONCTIONNEMENT ##############################
 
 print("Nombre de ville :", len(ville))
+
 print("Liste des distances entre 1ere ville et chaque ville :\n", listeDistance(ville))
+
 print("Numero de ville la plus proche de la 1ere ville :", numVillePlusProche(ville))
 
+print("Nombre des vrais chemins possibles ", len(cheminsPossibles))
+
+print("Nombre total des chemins avec chemins miroir ", len(list(permutations(ville[1:]))))
+
+print("Distance Totale du chemin [2]", calculDistanceTotaleChemin(cheminsPossibles[2]))
+
+
+
+
+
+
+
+
+
+## ToDo A supprimer ?
+parcours = []
+parcours.append(0)
+sauve = ville
 
 ## ToDo A supprimer ?
 zaza = numVillePlusProche(ville)
 print(ville[zaza])
 
 ## ToDo A supprimer ?
-chem = list(permutations(ville[1:]))
-cheminPossible= []
-for p in chem:
-    if p[0] < p[1]:
-        cheminPossible.append(p)
 
 
-############### TESTS DE FONCTIONNEMENT ##############################
 
-print("Nombre des vrais chemins possibles ", len(cheminPossible))
 
-print("Nombre total des chemins avec chemins miroir ", len(list(permutations(ville[1:]))))
 
-print("Distance Totale du chemin [2]", calculDistanceTotaleChemin(cheminPossible[2]))
+
+
+
+
 
