b393f34ea31e95d238dd7a13a71c02bdfb4bf96c
a2lino <araujo_adelino@orange.fr>
2019-05-09 23:09:40 +0200

    - ajout fonction permutation - ajout fonction liste Tous Chemins Possibles

--- fonctionsSolutionOptimale.py
+++ fonctionsSolutionOptimale.py
@@ -54,25 +54,21 @@ def listeDistance(ville):
     return listeDesDistances
 
 
-# FONCTION
-# ensures : creer une liste de tous les chemins possibles, sans mirroir, avec la ville de depart en debut et fin de chaque chemin
-# param   : liste des villes fichier csv
-# return  : type liste de liste 
-def listeCompleteChemin(ville):
-    chem = list(permutations(ville[1:]))
-    cheminPossible= []
-    for p in chem:
+#FONCTION
+# ensures : Création de la liste de tous les chemins possibles et de leur nombre
+# param   : liste des villes (tuples) créées par la fonction correspondante
+# return dans l'ordre  : liste de tous les chemins possibles SANS miroir | nombre de chemins possibles
+def listeCheminsPossibles (ville) :
+    chemins = list(permutations(ville[1:]))
+    cheminsPossibles = []
+    for p in chemins :
         if p[0] < p[1]:
-            cheminPossible.append(p)
+            cheminsPossibles.append(p)
 
-    cheminPossible = list(cheminPossible)
+    nombreCheminsPossibles = len(cheminsPossibles)
 
-    for i, chemin in enumerate(cheminPossible):
-        chemin = list(chemin)
-        chemin.insert(0, ville[0])
-        chemin.append(ville[0])
+    return cheminsPossibles, nombreCheminsPossibles
 
-    return cheminPossible
 
 
 # FONCTION
