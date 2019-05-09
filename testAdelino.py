b918d0faed183badf1eede1230e7ca2a74f3c77c
a2lino <araujo_adelino@orange.fr>
2019-05-09 22:24:39 +0200

    Correction

--- testAdelino.py
+++ testAdelino.py
@@ -1,6 +1,16 @@
-from mainSolutionOptimale import *
 from fonctionsSolutionOptimale import *
 
 
 
+def ouvrirFichierCSV (fichiercsv) :
+    ville = []
+    with open(fichiercsv) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            for coord in row:
+                coord = float(coord)
 
+            ville.append(row)
+
+    return ville
--- testAntoine.py
+++ testAntoine.py
@@ -1,2 +1,1 @@
-from mainSolutionOptimale import *
 from fonctionsSolutionOptimale import *
