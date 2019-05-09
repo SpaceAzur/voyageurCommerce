01b682a58eb6d4b5b52fd648e33675498603e866
a2lino <araujo_adelino@orange.fr>
2019-05-09 23:08:51 +0200

    - déplacement de la fonction en cours de test (insertion ville depart avant et fin liste chemins possibles)

--- testAntoine.py
+++ testAntoine.py
@@ -1,1 +1,22 @@
 from fonctionsSolutionOptimale import *
+
+
+# FONCTION
+# ensures : creer une liste de tous les chemins possibles, sans mirroir, avec la ville de depart en debut et fin de chaque chemin
+# param   : liste des villes fichier csv
+# return  : type liste de liste
+def listeCompleteChemin(ville):
+    chem = list(permutations(ville[1:]))
+    cheminPossible= []
+    for p in chem:
+        if p[0] < p[1]:
+            cheminPossible.append(p)
+
+    cheminPossible = list(cheminPossible)
+
+    for i, chemin in enumerate(cheminPossible):
+        chemin = list(chemin)
+        chemin.insert(0, ville[0])
+        chemin.append(ville[0])
+
+    return cheminPossible
