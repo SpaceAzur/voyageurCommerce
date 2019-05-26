6f545c3ed79280d469ae8525f1c63745d22e4713
a2lino <araujo_adelino@orange.fr>
2019-05-26 12:09:22 +0200

    Ajout code pour tester fonctionnement

--- mainSolutionOptimale.py
+++ mainSolutionOptimale.py
@@ -7,9 +7,32 @@ from fonctionsSolutionOptimale import *
 
 
 
-
 #################################################################
 ##################  DEBUT DU PROGRAMME ##########################
 #################################################################
 
+#mesage accueil
 
+
+import time
+from functools import wraps
+
+
+from fonctionsSolutionOptimale import *
+
+start_time = time.time()
+
+listeVilles = listeCoordonneesVillesFromCSV('data/test10.csv')
+print("")
+listeChemins = listeCheminsPossibles(listeVilles)
+print("")
+tableauDistances = calculDistancesCheminsPossibles(listeChemins)
+print("")
+cheminOptimal, distanceCheminOptimal = cheminOptimal(tableauDistances, listeChemins, listeVilles)
+print("")
+
+print("chemin optimal :", cheminOptimal)
+print("")
+print("distance chemin optimal :", distanceCheminOptimal)
+print("")
+print("--- %s seconds ---" % (time.time() - start_time))
