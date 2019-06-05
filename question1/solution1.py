1dc51bc55d88fd1cf170d543431242919a4c558e
a2lino <araujo_adelino@orange.fr>
2019-06-05 17:08:28 +0200

    Mise à jour du fichier pour presentation tp

--- question1/solution1.py
+++ question1/solution1.py
@@ -1,29 +1,43 @@
+from fonctions_main import *
 from fonctionsSolutionOptimale import *
-
 
 ##########################################
 #### SOLUTION 1 : utilise les listes #####
 ##########################################
 
+
+print("##################################################\n"
+      "################## SOLUTION 1 ####################\n"
+      "################################################## \n\n")
+
+
+#demande choix liste user
+choix_user = demande_choix_ville()
+
+#selection chemin fichier csv
+fichier_csv = selection_chemin_fichier_csv(choix_user)
+
 #debut chrono
+print("\nDépart du chrono ! ")
 start_time = time.time()
 
 #recuperation coordonnees villes
-liste_villes = liste_coordonnees_villes_from_csv('../data/test10.csv')
+liste_villes = liste_coordonnees_villes_from_csv(fichier_csv)
 
 #generation chemins hamiltoniens
-liste_chemins = liste_chemins_possibles(liste_villes)
+liste_chemins = liste_chemins_possibles_tuples_coordonnees(liste_villes)
 
-#calcul distances chemins hamiltoniens
-tableauDistances = calcul_distances_chemins_possibles(liste_chemins)
+#tableau distances chemins hamiltoniens
+tableau_distances = calcul_distances_chemins_possibles(liste_chemins)
+
 
 #calcul parcours optimal | distance parcours optimal
-cheminOptimal, distanceCheminOptimal = calcul_chemin_optimal(tableauDistances, liste_chemins, liste_villes)
+cheminOptimal, distanceCheminOptimal = calcul_chemin_optimal_methode_liste(tableau_distances, liste_chemins, liste_villes)
 
+chrono = (round((time.time() - start_time), 4))
 
-print("\n\nchemin optimal : ", cheminOptimal)
+print("\n\nLe chemin le plus court est  : ", convertion_chemin_liste_pour_user(cheminOptimal))
 
-print("\ndistance chemin optimal :", distanceCheminOptimal)
+print("\nSa distance est de ", round((distanceCheminOptimal), 4), "unités. ")
 
-
-print("\ntimer programme  %s seconds ---" % (time.time() - start_time))
+print("\nTemps d'exécution du programme : %s secondes " %chrono)
