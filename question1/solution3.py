72b67d649124c3475c2452d364ede83dca032757
escobar2018 <antoine.escobar@ensiie.fr>
2019-06-05 19:00:16 +0200

    question1 solu3 OK

--- question1/solution3.py
+++ question1/solution3.py
@@ -1,1 +1,46 @@
 #pour TSP
+from mpmath import *
+import numpy as np
+import matplotlib as plt
+from itertools import *
+import tsp, sys, gc, time, heapq, csv
+from fonctions_main import *
+from fonctionsSolutionOptimale import *
+
+##########################################
+#### SOLUTION 3 : utilise les listes #####
+##########################################
+
+
+print("##################################################\n"
+      "################## SOLUTION 3 ####################\n"
+      "################################################## \n\n")
+
+
+#demande choix liste user
+choix_user = demande_choix_ville()
+
+#selection chemin fichier csv
+fichier_csv = selection_chemin_fichier_csv(choix_user)
+
+#debut chrono
+start_time = time.time()
+
+#recuperation coordonnees villes
+liste_villes = liste_coordonnees_villes_from_csv(fichier_csv)
+
+#debut chrono
+start_time = time.time()
+
+#calcul distances entre villes
+matrice_distances = matrice_distances(liste_villes)
+
+chemin, distance = solutionOptimalTSP(matrice_distances)
+
+chrono = (round((time.time() - start_time), 4))
+
+print("\n\nLe chemin le plus court est  : ", chemin)
+
+print("\nSa distance est de ", distance, "unités. ")
+
+print("\nTemps d'exécution du programme : %s secondes " %chrono)
