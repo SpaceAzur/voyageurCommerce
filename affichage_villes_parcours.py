4ec4e528c6e0913358b13185e1005406bab0dd68
a2lino <araujo_adelino@orange.fr>
2019-06-22 16:13:31 +0200

    Ajout affichage des villes

--- affichage_villes_parcours.py
+++ affichage_villes_parcours.py
@@ -2,12 +2,11 @@ from fonctionsSolutionOptimale import *
 
 
 #recupere les coordonnes des villes (tuples de str)
-coordonnees_villes = liste_coordonnees_villes_from_csv('data/test10.csv')
+coordonnees_villes = liste_coordonnees_villes_from_csv('data/burma14.csv')
 
 #converti ces coordonnees en float (besoin graphique)
 coordonnees_float = convertion_liste_coordonnees_str_liste_coordonnes_float(coordonnees_villes)
 
-#affiche villes
 
 #USAGE
 #mettre 0 si affichage villes uniquement
@@ -17,4 +16,4 @@ coordonnees_float = convertion_liste_coordonnees_str_liste_coordonnes_float(coor
 #dessiner_parcours_voyageur_commerce(coordonnees_float, 0)
 
 #affichage ville + parcours
-dessiner_parcours_voyageur_commerce(coordonnees_float, '1-2-4-5-6-9-10-8-7-3-1')
+dessiner_parcours_voyageur_commerce(coordonnees_float, '7, 10, 8, 0, 1, 13, 11, 5, 6, 12, 2, 3, 9, 4')
